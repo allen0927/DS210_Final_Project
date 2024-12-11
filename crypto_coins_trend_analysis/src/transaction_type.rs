c9ec89228fc26c30150fe413812a4dd5d242fd40
allen0927 <allen20040207@gmail.com>
2024-12-11 14:34:56 -0500

    initialized type

---
+++ crypto_coins_trend_analysis/src/transaction_type.rs
@@ -0,0 +1,15 @@
+pub struct Transaction {
+  pub value: f64,           //value of transaction
+  pub unit: String,         //coin used
+  pub timestamp: u64,       //time of transaction, if time1 < time2, then time1 is earlier than time2
+}
+
+impl Transaction {
+  pub fn new(value: f64, unit:String, timestamp: u64) -> Self {
+      Transaction {
+          value,
+          unit,
+          timestamp,
+      }
+  }
+}
